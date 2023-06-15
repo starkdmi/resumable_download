4649b633ae63302c51d741c0ce05c2df24ad5a85
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-06-15 21:16:08 +0300

    Update download_task_example.dart

--- example/download_task_example.dart
+++ example/download_task_example.dart
@@ -5,19 +5,19 @@ void main() async {
   // specify url and destionation
   final url = Uri.parse("https://golang.org/dl/go1.19.1.src.tar.gz");
   final file = File("${Directory.current.path}/example/Go.tar.gz");
-  
+
   // initialize download request
   final task = await DownloadTask.download(url, file: file);
 
   // listen to state changes
   double previousProgress = 0.0;
-  task.events.listen((event) { 
+  task.events.listen((event) {
     switch (event.state) {
       case TaskState.downloading:
         final bytesReceived = event.bytesReceived!;
         final totalBytes = event.totalBytes!;
         if (totalBytes == -1) return;
-        
+
         final progress = (bytesReceived / totalBytes * 100).floorToDouble();
         if (progress != previousProgress && progress % 10 == 0) {
           print("progress $progress%");
